636a916e068d6bf532d69543546c27484b670717
Cedric David <cedric.david@btopenworld.com>
2018-09-02 13:10:53 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,9 +9,25 @@ def my_all?(arr)
     i = i + 1
   end
 
-  if collection.include?(false)
-    false
-  else
-    true
+  # if collection.include?(false)
+  #   false
+  # else
+  #   true
+  # end
+
+  # collection.each {|i|
+  #   if i == false
+  #     return false
+  #   end
+  #   }
+  # true
+  j = 0
+  res = true
+  while j < collection.size do
+    if collection[j] == false
+      res = false
+    end
+    j += 1
   end
+  return res
 end
